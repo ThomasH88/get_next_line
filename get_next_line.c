a28c852ef371a0f093318ebf9d882db4b3f6cac0
Thomas Holzheu <tholzheu@e1z2r5p43.42.us.org>
2018-10-11 12:03:17 -0700

    k

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: tholzheu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/09 19:39:41 by tholzheu          #+#    #+#             */
-/*   Updated: 2018/10/10 22:36:05 by tholzheu         ###   ########.fr       */
+/*   Updated: 2018/10/11 12:02:26 by tholzheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+//#include <stdio.h>//kasjlfjsjf
 
 char	*str_concat(char *prev, char *buf, ssize_t n)
 {
@@ -46,26 +47,35 @@ int		get_next_line(const int fd, char **line)
 	char			*file;
 	static char		**arr;
 
-	if (!line)
-		return (-1);
 	if (arr == NULL)
 	{
 		if (!(buf = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)))
 			return (-1);
 		file = NULL;
+		br = read(fd, buf, BUFF_SIZE);
+		//printf("buf = %s\n", buf);
+		//printf("bytes read = %ld\n", br);
+		file = str_concat(file, buf, br);
+		//printf("file prog = %s\n", file);
 		while ((br = read(fd, buf, BUFF_SIZE)))
 		{
+			//printf("right here\n");
 			if (br == -1)
 				return (-1);
 			file = str_concat(file, buf, br);
+			//printf("file prog = %s\n", file);
 		}
+		//printf("file final = %s\n", file);
 		arr = ft_strsplit((char const *)file, '\n');
 		ft_strdel(&buf);
 		ft_strdel(&file);
 	}
+	//if (arr == NULL)
+		//printf("wtf\n");
 	if (*arr == NULL)
 		return (0);
 	*line = *arr;
+	//printf("line = %s\n", *line);
 	(void)*(arr)++;
 	return (1);
 }
