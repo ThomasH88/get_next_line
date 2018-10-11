6dea76f56df0714b3a1fa9d1385065ab51b3fbe9
Thomas Holzheu <tholzheu@e1z2r5p43.42.us.org>
2018-10-11 12:42:02 -0700

    v 2.0

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tholzheu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/09 19:39:41 by tholzheu          #+#    #+#             */
-/*   Updated: 2018/10/11 12:38:10 by tholzheu         ###   ########.fr       */
+/*   Updated: 2018/10/11 12:41:34 by tholzheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,8 @@ int		get_next_line(const int fd, char **line)
 	char			*file;
 	static char		**arr;
 
+	if (!line)
+		return (-1);
 	if (arr == NULL)
 	{
 		if (!(buf = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)))
